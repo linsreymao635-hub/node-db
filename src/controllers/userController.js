63c4014fa865d51626c6d9876350f426ff145c88
linsreymao635-hub <linsreymao635@gmail.com>
2026-04-30 13:04:13 +0700

    push code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,63 +1,47 @@
-import { BaseController } from "./baseController.js";
-import User from "../models/user.js";
-
-class UserController extends BaseController {
-  constructor() {
-    super();
-  }
-
-  getUsers = async (req, res) => {
+import User from "../models/User.js";
+export class userController extends BaseController {
+  // GET ALL users
+  usersList = async (req, res) => {
     try {
-      const users = await User.getAll();
+      const users = await userController.getAll();
       this.success(res, "Users retrieved successfully", users);
     } catch (error) {
-      this.error(res, error.message || "Unable to retrieve users", 500);
+      this.error(res, error.message, 500);
     }
-  };
+  }
 
-  createUser = async (req, res) => {
+  // CREATE user
+  createUser = async (res, req) => {
     const name = req.body.name;
-    try {
-      const user = await User.create(name);
+    try{
+      const user = await user.create(name);
       this.success(res, "User created successfully", user, 201);
     } catch (error) {
-      this.error(res, error.message || "Unable to create user", 500);
+      this.error(res, error.message, 500);
     }
-  };
+  }
 
-  getUserById = async (req, res) => {
-    const id = req.params.id;
-    try {
-      const user = await User.getById(id);
-      if (!user) return this.error(res, "User not found", 404);
-      this.success(res, "User retrieved successfully", user);
-    } catch (error) {
-      this.error(res, error.message || "Unable to retrieve user", 500);
-    }
-  };
-
-  updateUser = async (req, res) => {
+  // UPDATE user
+  updateUser = async (res, req) => {
     const id = req.params.id;
     const name = req.body.name;
-    try {
-      const user = await User.update(id, name);
-      if (!user) return this.error(res, "User not found", 404);
+    try{
+      const user = await user.update(id, name);
       this.success(res, "User updated successfully", user);
     } catch (error) {
-      this.error(res, error.message || "Unable to update user", 500);
+      this.error(res, error.message, 500);
     }
-  };
+  }
 
-  deleteUser = async (req, res) => {
+  // DELETE user
+  deleteUser = async (res, req) => {
     const id = req.params.id;
-    try {
-      const deleted = await User.delete(id);
-      if (!deleted) return this.error(res, "User not found", 404);
+    try{
+      await user.delete(id);
       this.success(res, "User deleted successfully");
     } catch (error) {
-      this.error(res, error.message || "Unable to delete user", 500);
+      this.error(res, error.message, 500);
     }
-  };
-}
+  }
 
-export default new UserController();
+}
